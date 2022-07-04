e05f7971a3177f5800b96dd95ade2f90820b8afa
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-03 21:21:18 -0400

    Updates!

--- Commands/Test.hpp
+++ Commands/Test.hpp
@@ -41,17 +41,36 @@ namespace DiscordCoreAPI {
 
 		void execute(BaseFunctionArguments& argsNew) {
 			try {
-				RespondToInputEventData dataPackage{ argsNew.eventData };
-				dataPackage.addContent("The FINAL ROLL: " + function());
-				dataPackage.setResponseType(InputEventResponseType::Interaction_Response);
-				auto newEvent = InputEvents::respondToInputEventAsync(dataPackage).get();
-				RespondToInputEventData dataPackage02{ newEvent };
-				for (int32_t x = 0; x < 50; x += 1) {
-					dataPackage02.addContent("The FINAL ROLL: " + function());
-					dataPackage02.setResponseType(InputEventResponseType::Ephemeral_Follow_Up_Message);
-					auto newEvent = InputEvents::respondToInputEventAsync(dataPackage02).get();
+				GuildMember guildMember =
+					GuildMembers::getCachedGuildMemberAsync({ .guildMemberId = argsNew.eventData.getAuthorId(), .guildId = argsNew.eventData.getGuildId() }).get();
+				Guild guild = Guilds::getCachedGuildAsync({ .guildId = argsNew.eventData.getGuildId() }).get();
+				DiscordGuild discordGuild{ guild };
+				VoiceStateData voiceStateData{};
+				if (guild.voiceStates.contains(guildMember.id)) {
+					voiceStateData = guild.voiceStates.at(guildMember.id);
+				} else {
+					std::unique_ptr<DiscordCoreAPI::EmbedData> newEmbed{ std::make_unique<DiscordCoreAPI::EmbedData>() };
+					newEmbed->setAuthor(argsNew.eventData.getUserName(), argsNew.eventData.getAvatarUrl());
+					newEmbed->setDescription("------\n__**Sorry, but you need to be in a correct voice channel to issue those commands!**__\n------");
+					newEmbed->setTimeStamp(getTimeAndDate());
+					newEmbed->setTitle("__**Playing Issue:**__");
+					newEmbed->setColor(discordGuild.data.borderColor);
+					RespondToInputEventData dataPackage(argsNew.eventData);
+					dataPackage.setResponseType(InputEventResponseType::Follow_Up_Message);
+					dataPackage.addMessageEmbed(*newEmbed);
+					auto newerEvent = InputEvents::respondToInputEventAsync(dataPackage).get();
+					dataPackage.setResponseType(InputEventResponseType::Ephemeral_Follow_Up_Message);
+					newerEvent = InputEvents::respondToInputEventAsync(dataPackage).get();
+					InputEvents::deleteInputEventResponseAsync(argsNew.eventData).get();
+					InputEvents::deleteInputEventResponseAsync(newerEvent, 20000);
+					return;
 				}
 
+				for (uint32_t x = 0; x < 100; x += 1) {
+					VoiceConnection* voiceConnection = guild.connectToVoice(guildMember.id, 0, true, false);
+					std::this_thread::sleep_for(50ms);
+					guild.disconnect();
+				}
 
 
 				return;
