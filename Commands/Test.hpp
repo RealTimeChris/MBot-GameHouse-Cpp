65266717abd7a80242fd6662c7b5646200b1fb65
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-21 06:18:22 -0400

    Update Test.hpp

--- Commands/Test.hpp
+++ Commands/Test.hpp
@@ -7,21 +7,6 @@
 
 namespace DiscordCoreAPI {
 
-
-	std::vector<std::string> redNumbers{ ":red_square:32", ":red_square:19", ":red_square:21", ":red_square:25", ":red_square:34", ":red_square:27", ":red_square:36",
-		":red_square:30", ":red_square:23", ":red_square:5", ":red_square:16", ":red_square:1", ":red_square:14", ":red_square:9", ":red_square:18", ":red_square:7",
-		":red_square:12", ":red_square:3" };
-	std::vector<std::string> blackNumbers{ ":black_large_square:15", ":black_large_square:4", ":black_large_square:2", ":black_large_square:17", ":black_large_square:6",
-		":black_large_square:13", ":black_large_square:11", ":black_large_square:8", ":black_large_square:10", ":black_large_square:24", ":black_large_square:33",
-		":black_large_square:20", ":black_large_square:31", ":black_large_square:22", ":black_large_square:29", ":black_large_square:28", ":black_large_square:35",
-		":black_large_square:26" };
-
-	std::string function() {
-		std::mt19937_64 randomEngine{ static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count()) };
-		std::string finalRoll = std::to_string(static_cast<uint32_t>(static_cast<float>(randomEngine()) / static_cast<float>(randomEngine.max()) * 38.0f));
-		return getNumberString(finalRoll, redNumbers, blackNumbers);
-	}
-
 	class Test : public BaseFunction {
 	  public:
 		Test() {
